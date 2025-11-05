726a990865c1e2a2c9a0f96a938a9cc1c0e07342
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-06 02:21:47 +0500

    updating routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Layout from "./Layout/layout";
 import Home from "./Home-page/home";
 import Shop from "./Shop/Shop";
 import Admindashboard from "./Admin/admindashboard";
+import Auth from "./auth/auth";
+import ForgotPassword from "./auth/forgotpassword";
+
 
 function App() {
   return (
@@ -11,8 +14,11 @@ function App() {
         <Routes>
           {/* Layout Routes (with navbar, footer, etc.) */}
           <Route element={<Layout />}>
+          
             <Route path="/" element={<Home />}/> {/* ✅ Home loads first */}
             <Route path="/Home/shop" element={<Shop />} />
+            <Route path="/Home/Account" element={<Auth/>}/>
+            <Route path="/Account/Forgot/Password" element={<ForgotPassword/>}/>
           </Route>
 
           {/* Admin route (no layout) */}

ac558734e3db13d59e1dab1ca4198413a4696547
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-08 09:23:23 +0500

    removing commented code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Layout from "./Layout/layout";
-// import Home from "./Home-page/home";
-// import Shop from "./Shop/Shop";
-// import Admindashboard from "./Admin/admindashboard";
-// import Auth from "./auth/auth";
-// import ForgotPassword from "./auth/forgotpassword";
-
-
-// function App() {
-//   return (
-//     <div className="font-['Jost']">
-//       <BrowserRouter>
-//         <Routes>
-//           {/* Layout Routes (with navbar, footer, etc.) */}
-//           <Route element={<Layout />}>
-          
-//             <Route path="/" element={<Home />}/> {/* ✅ Home loads first */}
-//             <Route path="/Home/shop" element={<Shop />} />
-//             <Route path="/Home/Account" element={<Auth/>}/>
-//             <Route path="/Account/Forgot/Password" element={<ForgotPassword/>}/>
-//           </Route>
-
-//           {/* Admin route (no layout) */}
-//           <Route path="/admin/*" element={<Admindashboard/>} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/layout";
 import Home from "./Home-page/home";

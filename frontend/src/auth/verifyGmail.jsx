9787c5c606179c04fd3e268cf2fc9c81bb9a1a6f
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-11 17:21:44 +0500

    user click on gmail and get verified

--- frontend/src/auth/verifyGmail.jsx
+++ frontend/src/auth/verifyGmail.jsx
@@ -1,30 +1,32 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const VerifyGmail = () => {
-      const {token} = useParams();
-      const[status,setStatus] = useState("verifying....");
-      useEffect(()=>{
-           const verifyGmail = async()=>{
-            const res = await axios.post("http://localhost:3000/user/verify",{},{
-                  headers:{
-                        Authorization:`Bearer ${token}`
+      const { token } = useParams();
+      const [status, setStatus] = useState("verifying....");
+      const navigate = useNavigate();
+      useEffect(() => {
+            const verifyGmail = async () => {
+                  const res = await axios.post("http://localhost:3000/user/verify", {}, {
+                        headers: {
+                              Authorization: `Bearer ${token}`
+                        }
+                  });
+                  if (res.data.success) {
+                        setStatus("Email verified successfully");
+                        setTimeout(() => {
+                              navigate("/Home/Account");
+                        }, 1000);
                   }
-            });
-            if(res.data.success){
-                  setStatus("Email verified successfully");
-                  setTimeout(() => {
-                        
-                  }, 2000);
             }
-           } 
-      })
-  return (
-    <div>
-
-    </div>
-  )
+            verifyGmail();
+      }, [token, navigate])
+      return (
+            <div>
+                  {status}
+            </div>
+      )
 }
 
 export default VerifyGmail;

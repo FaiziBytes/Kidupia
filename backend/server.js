b86800909ba37689133c69b6ad2ba9f4c7e53a37
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-11 17:20:55 +0500

    adding cors origin and credentials

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ dotenv.config();
 const app = express();
 
 // 🔹 Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // or "*"
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));

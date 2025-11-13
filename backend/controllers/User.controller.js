2145ee8227d9422086898b4a9774bd9a20a9f911
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-13 22:42:55 +0500

    writing controller for the getting all the users in the db

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -303,3 +303,18 @@ export const changePassword = async (req, res)=>{
         })
     }
 }
+
+export const getAllUsers = async (req, res) => {
+      try {
+            const users = await UserModel.find();
+            return res.status(200).json({
+                  success: true,
+                  data: users
+            })
+      } catch (error) {
+            return res.status(500).json({
+                  success: false,
+                  message: error.message
+            })
+      }
+}

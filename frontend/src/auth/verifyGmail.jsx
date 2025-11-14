e118adebf5f1b7480bf735ce5e526984ee39d3ca
FaiziBytes <chfaizanaslam10@gmail.com>
2025-11-14 23:57:34 +0500

    removed all the errors in the verifyGmail.jsx

--- frontend/src/auth/verifyGmail.jsx
+++ frontend/src/auth/verifyGmail.jsx
@@ -1,42 +1,9 @@
-// import axios from 'axios';
-// import React, { useEffect, useState } from 'react'
-// import { useNavigate, useParams } from 'react-router-dom';
-
-// const VerifyGmail = () => {
-//       const { token } = useParams();
-//       const [status, setStatus] = useState("verifying....");
-//       const navigate = useNavigate();
-//       useEffect(() => {
-//             const verifyGmail = async () => {
-//                   const res = await axios.post("http://localhost:3000/user/verify", {}, {
-//                         headers: {
-//                               Authorization: `Bearer ${token}`
-//                         }
-//                   });
-//                   if (res.data.success) {
-//                         setStatus("Email verified successfully");
-//                         setTimeout(() => {
-//                               navigate("/Home/Account");
-//                         }, 1000);
-//                   }
-//             }
-//             verifyGmail();
-//       }, [token, navigate])
-//       return (
-//             <div>
-//                   {status}
-//             </div>
-//       )
-// }
-
-// export default VerifyGmail;
-
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const VerifyGmail = () => {
-  const { token } = useParams();
+  const {token} = useParams();
   const [status, setStatus] = useState("Verifying...");
   const navigate = useNavigate();
 
